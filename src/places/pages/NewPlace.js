8c79b945969b72cac3a1d61a3a39c374ba7b6659
Vasyl <williamme260790@gmail.com>
2020-03-21 14:21:01 +0200

    Added Main Header and Navlinks

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const NewPlace = () => {
-    return <h2>NewPlace works!!!</h2>;
-  };
+  return <h2>NewPlace Works!</h2>
+};
 
-  export default NewPlace;
+export default NewPlace;
